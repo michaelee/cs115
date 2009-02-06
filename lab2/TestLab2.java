dbdf6a07f8b29d56a6749b6859298183a2ef7ed8
Michael Lee <lee@soi2.org>
2009-02-06 09:44:17 -0600

    Adding files for lab 2

---
+++ lab2/TestLab2.java
@@ -0,0 +1,26 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TestLab2 {  
+  @Before
+  public void setUp() {
+  }
+
+  @After
+  public void tearDown() {
+  }
+  
+  @Test
+  public void t() {
+    assertTrue(true);
+  }
+
+  // @Test
+  // public void simpleTest() {
+  //   Square sq = new Square();
+  //   int length = sq.getLength();
+  //   assertTrue("Length not greater than 0", length > 0);
+  //   assertEquals("Perimeter incorrect", sq.getPerimeter(), 4*length);
+  //   assertEquals("Area incorrect", sq.getArea(), length*length);
+  // }
+}
