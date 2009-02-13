081c95d3bb1f927e02793997da3906d4267e3488
Michael Lee <lee@soi2.org>
2009-02-13 01:45:10 -0600

    Adding partial lab3 files

---
+++ lab3/SimpleMath.java
@@ -0,0 +1,31 @@
+import static java.lang.Math.*;
+
+public class SimpleMath {
+  
+  public int sumOfSquares (int x, int y) {
+    int result = 0;
+    
+    return result;
+  }
+  
+  public double average (double a, double b, double c, double d, double e) {
+    int result = 0.0;
+    
+    return result;    
+  }
+  
+  
+  public boolean divisibleByNine (int n) {
+    boolean result = (n % 9) == 0;
+    return result;
+  }
+
+  public double distance (double x1, double y1, double x2, double y2) {
+  }
+  
+  public long secondsInYears () {    
+  }
+  
+  public int age () {
+  }
+}
