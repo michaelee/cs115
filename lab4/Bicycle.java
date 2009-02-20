d9ba43673faf79b1304b5af3b212d056979c9089
Michael Lee <lee@soi2.org>
2009-02-20 15:18:21 -0600

    Adding lab4 files.

---
+++ lab4/Bicycle.java
@@ -0,0 +1,40 @@
+class Bicycle {
+  double wheel_diam; // diameter of the bicycle wheels (in inches)
+  double multiplier; // gear multiplier
+  double distance;   // total distance traveled (in miles)
+  int cadence = 0;   // current pedaling cadence (in rotations/min)
+  int gear = 1;      // current gearing
+  
+  public Bicycle(double diam, double mul) {
+    wheel_diam = diam;
+    multiplier = mul;
+    distance = 0.0;
+    cadence  = 0;
+    gear     = 1;
+  }
+
+  void changeCadence(int val) {
+    cadence = val;
+  }
+
+  void gearDown() {
+    gear = gear - 1;
+  }
+
+  void gearUp() {
+    gear = gear + 1;
+  }
+  
+  void pedal(int minutes) {
+    // your code goes here!
+  }
+  
+  void printState() {
+    System.out.format("Distance traveled: %.2f miles\n", distance);
+  }
+
+  public static void main (String[] args) {
+    Bicycle bike = new Bicycle(29.0, 1.25);
+    bike.printState();
+  }
+}
