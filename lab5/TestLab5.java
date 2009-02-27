5683bf273c70faebe693d886a0457613e66a2c5b
Michael Lee <lee@soi2.org>
2009-02-27 15:42:01 -0600

    Updating test for default constructor

--- lab5/TestLab5.java
+++ lab5/TestLab5.java
@@ -5,28 +5,53 @@ public class TestLab5 {
   final static double DELTA = 0.5;
   
   @Test
-  public void testBikes() {
+  public void testAccessors() {
+    Bicycle bike = new Bicycle(29.5, 1.5);
+    assertEquals(1, bike.getGear());
+    assertEquals(0, bike.getCadence());
+    bike.changeCadence(50);
+    bike.gearUp();
+    bike.gearUp();
+    assertEquals(3, bike.getGear());
+    assertEquals(50, bike.getCadence());    
+    bike.changeCadence(20);
+    bike.gearDown();
+    assertEquals(2, bike.getGear());
+    assertEquals(20, bike.getCadence());    
+  }
+  
+  @Test
+  public void testDistance() {
     Bicycle b1 = new Bicycle(63360/Math.PI, 1),
             b2 = new Bicycle(30.0, 1.5);
+
     b1.changeCadence(1);
     b1.pedal(1);
-    assertEquals(1, b1.getGear());
-    assertEquals(1, b1.getCadence());
     assertEquals(1, b1.getDistance(), DELTA);
     b1.changeCadence(2);
     b1.pedal(2);
-    assertEquals(1, b1.getGear());
-    assertEquals(2, b1.getCadence());
     assertEquals(5, b1.getDistance(), DELTA);
     
     b2.changeCadence(60);
     b2.gearUp();
     b2.gearUp();
-    assertEquals(3, b2.getGear());
     b2.pedal(10);
     assertEquals(4.02, b2.getDistance(), DELTA);
     b2.gearDown();
     b2.pedal(20);
     assertEquals(9.37, b2.getDistance(), DELTA);    
   }
+  
+  /* Remove this comment when you're ready to test your default constructor!
+  
+  @Test
+  public void testDefaultConstructor() {
+    Bicycle simpleBike = new Bicycle();
+    simpleBike.changeCadence(60);
+    simpleBike.gearUp();
+    simpleBike.pedal(10);
+    assertEquals(1.73, simpleBike.getDistance(), DELTA);
+  }
+  
+  */
 }
